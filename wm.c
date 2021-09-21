20f327c1f60c9ad4089a594f964cd85feeece7d9
Murtaza Udaipurwala <murtaza.u6e24@gmail.com>
2021-09-21 23:15:43 +0530

    start and stop the connection with the xserver

---
+++ wm.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <X11/Xlib.h>
+
+static void start(void);
+static void stop(void);
+
+static Display *dpy;
+
+void start(void) {
+    if (!(dpy = XOpenDisplay(NULL))) {
+        fprintf(stderr, "Failed to establish a connection with the xserver\n");
+        exit(EXIT_FAILURE);
+    }
+    fprintf(stdout, "Connected to the xserver\n");
+}
+
+void stop(void) {
+    XCloseDisplay(dpy);
+    fprintf(stdout, "Disconnected from the xserver\n");
+}
+
+int main(void) {
+    start();
+    stop();
+    return 0;
+}
